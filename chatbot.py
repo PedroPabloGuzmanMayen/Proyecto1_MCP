5859d02b902cde74ae6839c75f2f17fce6d1ea8f
ppguz <ppguzmanmayen@gmail.com>
2025-08-24 00:03:27 -0600

    Feat: model have context

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,19 @@
 import anthropic
 
 client = anthropic.Anthropic()
-
+conversation = []
 
 while True:
 
     user_message = input("Ingresa tu mensaje: ")
+    conversation.append({"role": "user", "content": user_message})
     message = client.messages.create(
         model="claude-sonnet-4-20250514",
         max_tokens=1000,
-        messages=[
-            {
-                "role": "user",
-                "content": user_message
-            }
-        ]
+        messages= conversation
     )
-    print(message.content)
+
+    reply = message.content[0].text
+    print(reply)
+    
+    conversation.append({"role": "assistant", "content": reply})
